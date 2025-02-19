d3c7532b32130af5d634fc075aca24e0758a400d
Seyed Mahmoud Shahrokni <shahrokni.trivia@gmail.com>
2025-02-19 11:42:35 +0100

    refactor: Add messages to the test asseetion function

--- housefly-autopilot/test/test.c
+++ housefly-autopilot/test/test.c
@@ -4,23 +4,27 @@
 /* STATE MANAGEMENT TESTS */
 void should_reset_safety_test_flag() {
   char flags = 0x01;
-  TEST_ASSERT_EQUAL(flags, 0x01);
+  TEST_ASSERT_MESSAGE(flags == 0x01,
+                      "Should have set the flight safety flag to 1");
   reset_sftytstflg(&flags);
-  TEST_ASSERT_EQUAL(flags, 0x00);
+  TEST_ASSERT_MESSAGE(flags == 0x00,
+                      "Should have reset the flight safety flag");
 }
 
 void should_set_safety_test_flag() {
   char flags = 0x00;
-  TEST_ASSERT_EQUAL(flags, 0x00);
+  TEST_ASSERT_MESSAGE(flags == 0x00,
+                      "Should have reset the flight safety flag");
   set_sftytstflg(&flags);
-  TEST_ASSERT_EQUAL(flags, 0x01);
+  TEST_ASSERT_MESSAGE(flags == 0x01,
+                      "Should have set the flight safety flag to 1");
 }
 
 void should_reset_all_flags() {
   unsigned char flags = 0xff;
   TEST_ASSERT_EQUAL(flags, 0xff);
   reset_flags(&flags);
-  TEST_ASSERT_EQUAL(flags, 0x00);
+  TEST_ASSERT_MESSAGE(flags == 0x00, "Should have reset all flight flags");
 }
 
 void should_reset_flight_state() {
@@ -30,7 +34,7 @@ void should_reset_flight_state() {
   for (int i = 0; i < 7; i += 1) {
     FlightState state = states[i];
     reset_state(&state);
-    TEST_ASSERT_EQUAL(state, 0x00);
+    TEST_ASSERT_MESSAGE(state == 0x00, "Should have reset the flight state");
   }
 }
 
@@ -48,7 +52,7 @@ void should_validation_return_false_for_new_state_GND() {
     for (int j = 0; j < 256; j += 1) {
       int result = validate_requested_state(possible_invalid_current_states[i],
                                             new_state, j);
-      TEST_ASSERT_EQUAL(result, 0);
+      TEST_ASSERT_MESSAGE(result == 0, "Validation should return 0 (invalid)");
     }
   }
 }
@@ -62,7 +66,7 @@ void should_validation_return_true_for_new_state_GND() {
     for (int j = 0; j < 256; j += 1) {
       int result = validate_requested_state(possible_valid_current_states[i],
                                             new_state, j);
-      TEST_ASSERT_EQUAL(result, 1);
+      TEST_ASSERT_MESSAGE(result == 1, "Validation should return 1 (valid)");
     }
   }
 }
@@ -77,8 +81,25 @@ void should_validation_return_false_for_new_state_TO() {
     for (int j = 0; j < 256; j += 1) {
       int result = validate_requested_state(possible_invalid_current_states[i],
                                             new_state, j);
-      TEST_ASSERT_EQUAL(result, 0);
+      TEST_ASSERT_MESSAGE(result == 0, "Validation should return 0 (invalid)");
     }
+  }
+}
+
+/**
+ * @brief Although the transition from GND to TO is valid, since the safety test
+ * flag is not set, the validation should return 0
+ */
+void should_validation_return_false_fo_new_state_TO_wrong_flags() {
+  FlightState new_state = TO_STAT;
+  FlightState possible_valid_current_states[] = {GND_STAT};
+  int flags = 0x00;
+  int len = sizeof(possible_valid_current_states) / sizeof(int);
+
+  for (int i = 0; i < len; i += 1) {
+    int result = validate_requested_state(possible_valid_current_states[i],
+                                          new_state, flags);
+    TEST_ASSERT_MESSAGE(result == 0, "Validation should return 0 (invalid)");
   }
 }
 
@@ -92,7 +113,7 @@ void should_validation_return_true_for_new_state_TO() {
   for (int i = 0; i < len; i += 1) {
     int result = validate_requested_state(possible_valid_current_states[i],
                                           new_state, flags);
-    TEST_ASSERT_EQUAL(result, 1);
+    TEST_ASSERT_MESSAGE(result == 1, "Validation should return 1 (valid)");
   }
 }
 
@@ -108,6 +129,7 @@ int main() {
   RUN_TEST(should_validation_return_false_for_new_state_GND);
   RUN_TEST(should_validation_return_true_for_new_state_GND);
   RUN_TEST(should_validation_return_false_for_new_state_TO);
+  RUN_TEST(should_validation_return_false_fo_new_state_TO_wrong_flags);
   RUN_TEST(should_validation_return_true_for_new_state_TO);
   return UNITY_END();
 }
